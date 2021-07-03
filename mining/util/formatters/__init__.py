8fdfc9a203235e4cbd14794f2699bc3b2e850a86
Gary Miguel <garymiguel@microsoft.com>
2021-07-03 00:38:33 +0000

    Use a default file format if the file has no extension
    Also use `os.path.splitext` rather than string splitting. This is more
    robust.

--- mining/util/formatters/__init__.py
+++ mining/util/formatters/__init__.py
@@ -1,3 +1,6 @@
+import logging
+import os
+
 from typing import List, Optional
 from .Formatter import Formatter
 from .JsonFormatter import JsonFormatter
@@ -11,17 +14,22 @@ def get_valid_formatters() -> List[str]:
   return list(formatters.keys())
 
 
+_default_formatter: str = get_valid_formatters()[0]
+
+
 def get_formatter(cli_args) -> Optional[Formatter]:
   output_format = get_file_format(cli_args)
   if output_format in formatters:
     return formatters[output_format]()
 
 
-def get_file_format(cli_args):
-  if cli_args['fileformat'] == 'inferred':
-    if 'output' in cli_args:
-      return cli_args['output'].split('.')[-1]
-    if 'input' in cli_args:
-      return cli_args['input'].split('.')[-1]
+def get_file_format(cli_args) -> str:
+  path = cli_args.get('output', cli_args.get('input'))
+  if path and cli_args['fileformat'] == 'inferred':
+      _, ext = os.path.splitext(path)
+      if ext:
+        return ext[1:] # remove leading dot
+      logging.warn('could not detect file format from path, defaulting to %s', _default_formatter)
+      return _default_formatter
   else:
     return cli_args['fileformat']
